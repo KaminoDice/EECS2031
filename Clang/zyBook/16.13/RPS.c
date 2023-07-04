8d9e7fadd0f857d8b84e1e4c5fe0168ac2e36e0b
Sakura Tsuki <tsukitosakura@icloud.com>
2023-07-04 16:25:44 -0400

    calng lab 16.13

---
+++ Clang/zyBook/16.13/RPS.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+   const int ROCK = 0;
+   const int PAPER = 1;
+   const int SCISSORS = 2;
+
+   int seed;
+   scanf("%d", &seed);
+   srand(seed);
+
+   /* Type your code here. */
+
+   return 0;
+
+}
